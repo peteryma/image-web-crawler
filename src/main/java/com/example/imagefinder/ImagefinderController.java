9417e321bfdbe49c3ad8728a893c6e00073513f2
Peter Ma <peterma1999@gmail.com>
2023-11-24 22:35:12 -0500

    Only allow one request to process at a time

--- src/main/java/com/example/imagefinder/ImagefinderController.java
+++ src/main/java/com/example/imagefinder/ImagefinderController.java
@@ -1,14 +1,18 @@
 package com.example.imagefinder;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @RestController
 @RequestMapping("/imagefinder")
 public class ImagefinderController {
     private final ImagefinderService imagefinderService;
+    private final AtomicBoolean activeRequest = new AtomicBoolean(false);
 
     @Autowired
     public ImagefinderController(ImagefinderService imagefinderService) {
@@ -27,8 +31,17 @@ public class ImagefinderController {
 
 
     @PostMapping
-    public String[] searchUrl(@RequestParam("url") String url,
+    public ResponseEntity<String[]> searchUrl(@RequestParam("url") String url,
                               @RequestParam("depth") Integer depth) {
-        return imagefinderService.searchUrl(url, depth);
+        if (!activeRequest.compareAndSet(false, true)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                                 .body(new String[]{"Another request is currently being processed. Please try again later."});
+        }
+
+        try {
+            return ResponseEntity.ok(imagefinderService.searchUrl(url, depth));
+        } finally {
+            activeRequest.set(false);
+        }
     }
 }
